35ca6f7acd152fbb5b1c805c45dd4eb7186f50ce
liupu <178853518@qq.com>
2019-04-08 10:12:54 +0800

    update code for feature/components-and-props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 //React element都是不变的，当元素被创建后，你是无法改变其内容或属性的。
 //React 只会更新必要的部分
